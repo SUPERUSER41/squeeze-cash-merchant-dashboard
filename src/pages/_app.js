de61ed62c88e586f44029e941a70f097e26bcc78
Daneil <daneilgreaves@gmail.com>
2021-02-21 22:56:12 -0500

    Add tailwind

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,4 @@
-// eslint-disable-next-line import/no-unresolved
-import '../styles/globals.css';
+import 'tailwindcss/tailwind.css';
 
 function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1>Squeeze merchant</h1>
+      <h1 className="font-medium text-5xl">Squeeze merchant</h1>
     </div>
   );
 }
