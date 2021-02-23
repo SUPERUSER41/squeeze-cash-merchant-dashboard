0030f5ebe838316fd1af60d37dfe04018159d133
Daneil <daneilgreaves@gmail.com>
2021-02-22 21:38:32 -0500

    Complete scaffolding of header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,40 @@
-import { FiMenu, FiX } from 'react-icons/fi';
+import { FiMenu, FiX, FiBell } from 'react-icons/fi';
+import { AiOutlineShop } from 'react-icons/ai';
 
 const Header = () => {
   return (
-    <div className="flex items-center justify-between h-16 px-7 bg-gray-700">
-      <div className="flex items-center space-x-3 justify-between">
+    <div className="flex items-center justify-between h-16 px-7 bg-gray-darkest">
+      <div className="flex items-center space-x-7 justify-between">
         <button type="button">
-          <FiMenu className="text-gray-400 text-3xl" />
+          <FiMenu className="text-gray-lightest text-3xl" />
         </button>
-        <p className="font-normal text-2xl text-gray-400">Register</p>
+        <h1 className="font-normal text-2xl text-gray-lightest">Register</h1>
+        <button
+          type="button"
+          className="flex items-center space-x-1 rounded-full py-1 px-3 text-white bg-gray-light"
+        >
+          <p>New Orders</p>
+          <AiOutlineShop />
+        </button>
+        <button
+          type="button"
+          className="flex items-center space-x-1 rounded-full py-1 px-3 text-white bg-gray-light"
+        >
+          <AiOutlineShop />
+          <p>Rodney Bay</p>
+        </button>
+        <button
+          type="button"
+          className="flex items-center space-x-1 rounded-full py-1 px-3 text-white bg-gray-light"
+        >
+          <FiBell />
+          <p>17 New Alerts</p>
+        </button>
       </div>
-      <div className="flex items-center space-x-3 justify-between">
-        <p className="font-normal text-2xl text-gray-400">
+      <div className="flex items-center space-x-7 justify-between">
+        <h1 className="font-normal text-2xl text-gray-lightest">
           Tue., Jan. 15, 2021, 9:41 AM
-        </p>
+        </h1>
         <button type="button">
           <FiX className="text-white text-3xl" />
         </button>
