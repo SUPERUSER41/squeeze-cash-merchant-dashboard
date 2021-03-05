d4197abedf30d6ececb60e260e9e630588a4468f
Daneil <daneilgreaves@gmail.com>
2021-03-05 13:40:49 -0500

    Refactor menu items

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 module.exports = {
   distDir: 'build',
   reactStrictMode: true,
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,120 +1,148 @@
-import {
-  RiDashboardLine,
-  RiArrowRightSLine,
-  RiQrCodeLine,
-  RiSettings4Line,
-} from 'react-icons/ri';
-import { GoVerified } from 'react-icons/go';
+import Image from 'next/image';
+import Logo from '../../public/logo.svg';
+import SidePanelMenuItem from '../components/SidePanelMenuItem';
+import Icon from '../components/Icon';
+
+const MENUS = [
+  {
+    title: 'Dashboard',
+    iconFamily: 'Remix',
+    iconName: 'RiDashboardLine',
+  },
+  {
+    title: 'Point of Sale',
+    iconFamily: 'Ant',
+    iconName: 'AiOutlineAppstore',
+  },
+  {
+    title: 'Orders',
+    iconFamily: 'Remix',
+    iconName: 'RiShoppingBagLine',
+  },
+  {
+    title: 'Activity',
+    iconFamily: 'Remix',
+    iconName: 'RiBarChartFill',
+  },
+  {
+    title: 'Promotions',
+    iconFamily: 'Bootstrap',
+    iconName: 'BsFillLightningFill',
+  },
+  {
+    title: 'My Marketplace',
+    iconFamily: 'Ionicon',
+    iconName: 'IoPricetagOutline',
+  },
+  {
+    title: 'Rewards +',
+    iconFamily: 'Feather',
+    iconName: 'FiGift',
+  },
+  {
+    title: 'Bill Payments',
+    iconFamily: 'Remix',
+    iconName: 'RiBillLine',
+  },
+  {
+    title: 'Live Support',
+    iconFamily: 'Material',
+    iconName: 'MdLiveHelp',
+  },
+];
 
 export default function Home() {
   return (
     <div className="flex flex-row h-screen">
       <div className="flex flex-col justify-between w-96 px-5 py-5 bg-gray-darkest">
-        <header className="">
-          <div className="rounded-full bg-gray-lightest w-8 h-8">logo</div>
+        <header>
+          <Logo />
         </header>
         <div className="grid grid-cols-2 gap-4">
           <button
             type="button"
-            className="flex items-center justify-between px-4 py-5 bg-gray-light"
+            className="group flex items-center justify-between px-4 py-5 bg-gray-light hover:bg-green transition-colors duration-300"
           >
             <div className="flex items-start flex-col">
-              <p className="font-normal text-xs text-gray-lightest">
+              <p className="font-normal text-xs text-gray-lightest group-hover:text-white">
                 Select Store
               </p>
-              <p className="font-normal text-gray-lightest">Rodney Bay</p>
+              <p className="font-normal text-gray-lightest group-hover:text-white">
+                Rodney Bay
+              </p>
             </div>
-            <RiArrowRightSLine
-              className="align-middle mb-4 text-gray-lightest"
+
+            <Icon
+              family="Remix"
+              name="RiArrowRightSLine"
+              className="text-gray-lightest group-hover:text-white"
               size="30"
             />
           </button>
           <button
             type="button"
-            className="flex items-center justify-between px-4 py-5 bg-gray-light"
+            className="group flex items-center justify-between px-4 py-5 bg-gray-light hover:bg-green transition-colors duration-300"
           >
             <div className="flex items-start flex-col">
-              <p className="font-normal text-xs text-gray-lightest">
+              <p className="font-normal text-xs text-gray-lightest group-hover:text-white">
                 New Orders
               </p>
-              <p className="font-normal text-gray-lightest">Available</p>
+              <p className="font-normal text-gray-lightest group-hover:text-white">
+                Available
+              </p>
             </div>
+            <Icon
+              family="Remix"
+              name="RiArrowRightSLine"
+              className="text-gray-lightest group-hover:text-white"
+              size="30"
+            />
           </button>
-          <button
-            type="button"
-            className="flex flex-col items-center justify-center py-5 bg-green"
-          >
-            <RiDashboardLine className="mb-4 text-white" size="30" />
-            <p className="font-medium text-white">Dashboard</p>
-          </button>
-          <button
-            type="button"
-            className="flex flex-col items-center justify-center py-5 bg-gray-light"
-          >
-            <RiDashboardLine className="mb-4 text-white" size="30" />
-            <p className="font-medium text-white">Dashboard</p>
-          </button>
-          <button
-            type="button"
-            className="flex flex-col items-center justify-center py-5 bg-gray-light"
-          >
-            <RiDashboardLine className="mb-4 text-white" size="30" />
-            <p className="font-medium text-white">Dashboard</p>
-          </button>
-          <button
-            type="button"
-            className="flex flex-col items-center justify-center py-5 bg-gray-light"
-          >
-            <RiDashboardLine className="mb-4 text-white" size="30" />
-            <p className="font-medium text-white">Dashboard</p>
-          </button>
-          <button
-            type="button"
-            className="flex flex-col items-center justify-center py-5 bg-gray-light"
-          >
-            <RiDashboardLine className="mb-4 text-white" size="30" />
-            <p className="font-medium text-white">Dashboard</p>
-          </button>
-          <button
-            type="button"
-            className="flex flex-col items-center justify-center py-5 bg-gray-light"
-          >
-            <RiDashboardLine className="mb-4 text-white" size="30" />
-            <p className="font-medium text-white">Dashboard</p>
-          </button>
-          <button
-            type="button"
-            className="flex flex-col items-center justify-center py-5 bg-gray-light"
-          >
-            <RiDashboardLine className="mb-4 text-white" size="30" />
-            <p className="font-medium text-white">Dashboard</p>
-          </button>
-          <button
-            type="button"
-            className="flex flex-col items-center justify-center py-5 bg-gray-light"
-          >
-            <RiDashboardLine className="mb-4 text-white" size="30" />
-            <p className="font-medium text-white">Dashboard</p>
-          </button>
-          <button
-            type="button"
-            className="flex flex-col items-center justify-center py-5 bg-gray-light"
-          >
-            <RiDashboardLine className="mb-4 text-white" size="30" />
-            <p className="font-medium text-white">Dashboard</p>
-          </button>
+          {MENUS.map((menu) => (
+            <SidePanelMenuItem
+              key={menu.title}
+              title={menu.title}
+              icon={
+                <Icon
+                  family={menu.iconFamily}
+                  name={menu.iconName}
+                  className="mb-4 text-gray-lightest group-hover:text-white"
+                  size="30"
+                />
+              }
+            />
+          ))}
         </div>
-        <footer className="flex border-t-3 border-gray">
-          <div className="flex items-center space-x-4">
+        <footer className="flex items-center justify-between pt-4 border-t-4 border-gray">
+          <div className="flex items-center justify-between space-x-3">
+            <Image
+              src="/burgerking.png"
+              alt="merchant logo"
+              width="35"
+              height="35"
+            />
             <div>
               <div className="flex items-center space-x-2">
-                <p className="text-white">@burgerking</p>
-                <GoVerified className="text-blue" />
+                <p className="text-xs text-white">@burgerking</p>
+                <Icon family="Go" name="GoVerified" className="text-blue" />
               </div>
-              <p className="text-white">Andre Rolland</p>
+              <p className="text-xs text-white">Andre Rolland</p>
             </div>
-            <RiSettings4Line className="text-gray-lightest" size="30" />
-            <RiQrCodeLine className="text-gray-lightest" size="30" />
+          </div>
+
+          <div className="flex items-center space-x-4">
+            <Icon
+              family="Remix"
+              name="RiSettings4Line"
+              className="text-gray-lightest"
+              size="30"
+            />
+            <Icon
+              family="Remix"
+              name="RiQrCodeLine"
+              className="text-gray-lightest"
+              size="30"
+            />
             <button
               type="button"
               className="font-medium bg-gray-lightest px-3 py-1"
@@ -124,7 +152,7 @@ export default function Home() {
           </div>
         </footer>
       </div>
-      <div className="flex-grow bg-green">main content</div>
+      <div className="flex-grow bg-white">Tue., Jan. 15, 2021, 9:41 AM</div>
     </div>
   );
 }
